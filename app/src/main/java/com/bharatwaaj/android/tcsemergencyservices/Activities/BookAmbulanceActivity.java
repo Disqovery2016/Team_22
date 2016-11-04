ba27467815142176e17203737a4ab8ce89ed51c6
bharatwaaj <bharatwaaj.1996@gmail.com>
2016-11-04 12:45:30 +0530

    Login Server side code was developed

---
+++ app/src/main/java/com/bharatwaaj/android/tcsemergencyservices/Activities/BookAmbulanceActivity.java
@@ -0,0 +1,15 @@
+package com.bharatwaaj.android.tcsemergencyservices.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.bharatwaaj.android.tcsemergencyservices.R;
+
+public class BookAmbulanceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_ambulance);
+    }
+}
